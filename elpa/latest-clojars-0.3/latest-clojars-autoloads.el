d01c3067c934af9be63217d755de3bf18598729f
David James Humphreys <davidjameshumphreys@gmail.com>
2014-02-27 21:46:15 +0000

    The elpa packages as they are installed.

---
+++ elpa/latest-clojars-0.3/latest-clojars-autoloads.el
@@ -0,0 +1,29 @@
+;;; latest-clojars-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (insert-clojars) "latest-clojars" "latest-clojars.el"
+;;;;;;  (20867 54988))
+;;; Generated autoloads from latest-clojars.el
+
+(autoload 'insert-clojars "latest-clojars" "\
+
+
+\(fn PACKAGE INJECT\\?)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("latest-clojars-pkg.el") (20867 54988
+;;;;;;  953803))
+
+;;;***
+
+(provide 'latest-clojars-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; latest-clojars-autoloads.el ends here
---
+++ elpa/latest-clojars-0.3/latest-clojars-pkg.el
@@ -0,0 +1,1 @@
+(define-package "latest-clojars" "0.3" "Clojure dependency resolver" (quote nil))
